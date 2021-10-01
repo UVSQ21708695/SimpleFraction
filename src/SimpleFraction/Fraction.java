be53732c4dfa629bdcf2c9ae9d24d80bc9069658
Allaye DIALLO <allaye94@gmail.com>
2021-10-01 19:43:55 +0200

    premier commit

---
+++ src/SimpleFraction/Fraction.java
@@ -0,0 +1,21 @@
+package SimpleFraction;
+
+public class Fraction {
+	private int numerateur;
+	private int denominateur;
+	public Fraction(int num, int den) {
+		if(den>0) {
+			numerateur = num;
+			denominateur = den;
+		}
+		else if(den<0) {
+			numerateur = -1*num;
+			denominateur = -1*den;
+		}
+		else return;
+	}
+	@Override
+	public String toString() {
+		return numerateur +"/"+denominateur;
+	}
+}
---
+++ src/SimpleFraction/Main.java
@@ -0,0 +1,11 @@
+package SimpleFraction;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fraction frac = new Fraction(10,3);
+		System.out.println(frac.toString());
+	}
+
+}
