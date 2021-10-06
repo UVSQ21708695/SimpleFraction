e87c8eb47fc360cec8430a2d7eb779614fea9656
Allaye DIALLO <allaye94@gmail.com>
2021-10-06 14:31:10 +0200

    fin partie2

--- src/SimpleFraction/Fraction.java
+++ src/SimpleFraction/Fraction.java
@@ -3,19 +3,20 @@ package SimpleFraction;
 public class Fraction {
 	private int numerateur;
 	private int denominateur;
+
 	public Fraction(int num, int den) {
-		if(den>0) {
+		if (den > 0) {
 			numerateur = num;
 			denominateur = den;
-		}
-		else if(den<0) {
-			numerateur = -1*num;
-			denominateur = -1*den;
-		}
-		else return;
+		} else if (den < 0) {
+			numerateur = -1 * num;
+			denominateur = -1 * den;
+		} else
+			return;
 	}
+
 	@Override
 	public String toString() {
-		return numerateur +"/"+denominateur;
+		return numerateur + "/" + denominateur;
 	}
 }
